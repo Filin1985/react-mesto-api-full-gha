c33800c199ff59b9895ee47af8f1255b4d776b4f
Marat Ikhsanov <ihsmen@gmail.com>
2023-08-04 13:57:41 +0300

    Исправить замечания после ревью.

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -17,9 +17,7 @@ router.post('/', celebrate({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string()
       .required()
-      .pattern(new RegExp(URl_VALIDATOR_REG_EXP))
-      .min(2)
-      .max(30),
+      .pattern(new RegExp(URl_VALIDATOR_REG_EXP)),
   }),
 }), createNewCard);
 router.delete('/:cardId', celebrate({
